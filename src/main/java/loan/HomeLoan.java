95281fa800903d64d73a3f812c05136e14e96a1d
Akshay Shinde <akshay.shinde9522@gmail.com>
2024-02-29 13:06:10 +0530

    first

---
+++ src/main/java/loan/HomeLoan.java
@@ -0,0 +1,14 @@
+package loan;
+
+import org.testng.annotations.Test;
+
+public class HomeLoan {
+     @Test
+	 void homeLoanEligibility() {
+		 System.out.println("you are eligible");
+	 }
+     @Test
+	 void homeLoanSanction() {
+		 System.out.println("Congratulation");
+	 }
+}
